69dde56395e25f0b8d05f954ec553b731e476bc7
colabearwd <wz_jxnu@163.com>
2019-04-20 12:52:03 +0800

    update autochange

--- autochange.sh
+++ autochange.sh
@@ -1,4 +1,7 @@
 grep -Ilr push_url  ./ | xargs -n1 -- sed -i 's/1.1.1.1/1.1.1.1/g'
+grep -Ilr push_url  ./ | xargs -n1 -- sed -i 's/mysql_account/root/g'
+grep -Ilr push_url  ./ | xargs -n1 -- sed -i 's/mysql_passwd/passwd/g'
+grep -Ilr push_url  ./ | xargs -n1 -- sed -i 's/mysql_database/myproject/g'
 
 chmod +x get-args/*
 chmod +x rasp-plugin/*
